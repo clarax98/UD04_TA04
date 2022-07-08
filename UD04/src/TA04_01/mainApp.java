fa510f23278bb59ca2c31aab7420d219ddfb6544
Clara Ogalla <claraogalla@gmail.com>
2022-07-08 09:03:39 +0200

    TA04_01

--- UD04/src/TA04_01/mainApp.java
+++ UD04/src/TA04_01/mainApp.java
@@ -4,7 +4,23 @@ public class mainApp {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		double a = 1, b = 5;
+		//Suma, Resta, Multiplicacion, Division, Modulo
 		
+		//Suma
+		System.out.println("Suma: "+(a+b));
+		
+		//Resta
+		System.out.println("Suma: "+(a-b));
+		
+		//Multiplicacion
+		System.out.println("Suma: "+(a*b));
+		
+		//Division
+		System.out.println("Suma: "+(a/b));
+		
+		//Modulo
+		System.out.println("Suma: "+(a%b));
 	}
 
 }
