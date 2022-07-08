0529ce3c97f7aaec74c93ff378351ec951c51637
Clara Ogalla <claraogalla@gmail.com>
2022-07-08 09:16:51 +0200

    TA04_02

--- UD04/src/TA04_02/mainApp.java
+++ UD04/src/TA04_02/mainApp.java
@@ -4,7 +4,17 @@ public class mainApp {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		int n = 15;
+		double a = 32.25;
+		char c = 'c';
 		
+		System.out.println("N: "+n);
+		System.out.println("A: "+a);
+		System.out.println("C: "+c);
+		
+		System.out.println(n+" + "+a+" = "+(n+a));
+		System.out.println(a+" - "+n+" = "+(a-n));
+		System.out.print("Valor numérico del carácter "+c+" = "+ (int)c );
 	}
 
 }
