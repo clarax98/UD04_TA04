751dea4e1f524b20db9219928be81c8f5904042f
Clara Ogalla <claraogalla@gmail.com>
2022-07-10 15:32:46 +0200

    TA04_04

---
+++ UD04/src/TA04_04/mainApp.java
@@ -0,0 +1,13 @@
+package TA04_04;
+
+public class mainApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int n = 1;
+		System.out.println("Valor inicial de N = "+n);
+		System.out.println("N + 77 = "+(n = n+77));
+		System.out.println("N - 3 = "+(n = n - 3));
+		System.out.println("N * 2 = "+(n = n * 2));
+	}
+}
