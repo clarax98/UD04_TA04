b4e0e4f06922b9f0312583e5119a50ab759dc2b0
Clara Ogalla <claraogalla@gmail.com>
2022-07-10 15:27:58 +0200

    TA04_03

--- UD04/src/TA04_03/mainApp.java
+++ UD04/src/TA04_03/mainApp.java
@@ -4,7 +4,37 @@ public class mainApp {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		//Variables generales
+		int x = 2, y = 34;
+		double n = 98.5, m = 34.2;
+		
+		//Mostrar por pantalla los valores
+		System.out.println("Valor de X: "+x);
+		System.out.println("Valor de Y: "+y);
+		System.out.println("Valor de N: "+n);
+		System.out.println("Valor de M: "+m);
 
+		//Operar con los valores
+		System.out.println("La Suma de X + Y: "+(x+y));
+		System.out.println("La Diferencia de X - Y: "+(x-y));
+		System.out.println("El producto de X * Y: "+(x*y));
+		System.out.println("El cociente de X / Y: "+(x/y));
+		System.out.println("La suma de N + M: "+(n+m));
+		System.out.println("La diferencia de N - M: "+(n-m));
+		System.out.println("El producto de N * M: "+(n*m));
+		System.out.println("El cociente de N / M: "+(n/m));
+		System.out.println("El resto de N % M: "+(n%m));
+		System.out.println("La suma der X + N: "+(x+n));
+		System.out.println("El cociente de Y / M: "+(y/m));
+		System.out.println("El resto de Y % M: "+(y%m));
+		System.out.println("El doble de x: "+(x*2));
+		System.out.println("El doble de Y: "+(y*2));
+		System.out.println("El doble de N: "+(n*2));
+		System.out.println("El doble de M: "+(m*2));
+		System.out.println("La suma de X, Y, M, M: "+(x+y+n+m));
+		System.out.println("El producto de X, Y, M, M: "+(x*y*n*m));
+		
+	
 	}
 
 }
